6427278d87069c1c7b2743c20df89656524b2964
AntonioSalieri21 <62499991+AntonioSalieri21@users.noreply.github.com>
2024-05-25 06:43:14 +0200

    Skidish

--- main.cpp
+++ main.cpp
@@ -16,27 +16,27 @@ int main(int argc, char *argv[])
     int gridWidth = std::stoi(argv[2]);
     int gridHeight = std::stoi(argv[3]);
     //string path = "../Tiles/" + std::string(argv[1]) + "/config.json";
-    string path = std::string(argv[1]);
+    string path = std::string(argv[1]) + "/config.json";
     Tileset ts(path);
     
     unique_ptr<Grid> grid;
-    do {
+    // do {
 
-        grid.reset(new Grid(ts, gridWidth, gridHeight));
+    //     grid.reset(new Grid(ts, gridWidth, gridHeight));
 
-        WFCGenerator generator(*grid, ts);
-        auto start = std::chrono::high_resolution_clock::now();
+    //     WFCGenerator generator(*grid, ts);
+    //     auto start = std::chrono::high_resolution_clock::now();
 
-        generator.collapseGrid();
+    //     generator.collapseGrid();
 
-        auto end = std::chrono::high_resolution_clock::now(); 
-        std::chrono::duration<double> elapsed = end - start;
+    //     auto end = std::chrono::high_resolution_clock::now(); 
+    //     std::chrono::duration<double> elapsed = end - start;
 
-        std::cout << "Time taken to collapse grid: " << elapsed.count() << " seconds\n";
-        std::cout << std::endl;
+    //     std::cout << "Time taken to collapse grid: " << elapsed.count() << " seconds\n";
+    //     std::cout << std::endl;
         
-    } while(!grid->isValid());
-    ImageGenerator::saveImage(*grid, ts, "image.png");
+    // } while(!grid->isValid());
+    //ImageGenerator::saveImage(*grid, ts, "image.png");
     
     return 0;
 }
--- tilesetconfig.cpp
+++ tilesetconfig.cpp
@@ -1,6 +1,17 @@
 #include "tilesetconfig.h"
 #include <fstream>
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <unordered_map>
+#include <filesystem>
+
+struct MatCompare
+{
+    bool operator()(const cv::Mat& a, const cv::Mat& b) const
+    {
+        return cv::norm(a, b, cv::NORM_L1) != 0;
+    }
+};
 
 TilesetConfig::TilesetConfig(string config_path)
 {
@@ -13,6 +24,31 @@ TilesetConfig::TilesetConfig(string config_path)
     }
 
     file >> config;
+}
+
+vector<vector<cv::Mat>> splitImageIntoTiles(cv::Mat& img, int tileWidth, int tileHeight)
+{
+    vector<vector<cv::Mat>> tiles;
+    int counter = 0;
+    for(int y = 0; y < img.rows; y += tileHeight)
+    {
+        vector<cv::Mat> row;
+
+        for(int x = 0; x < img.cols; x += tileWidth)
+        {
+            cv::Rect roi(x, y, tileWidth, tileHeight);
+            cv::Mat tile = img(roi).clone(); // Ensure the tile is a continuous matrix
+            row.push_back(tile);
+
+            // Save each tile as a separate image
+            std::string filename = "tile_" + std::to_string(counter) + ".png";
+            cv::imwrite(filename, tile);
+            counter++;
+        }
+        tiles.push_back(row);
+    }
+
+    return tiles;
 }
 
 vector<TileInfo> TilesetConfig::getTilesInfo()
@@ -37,18 +73,60 @@ vector<TileInfo> TilesetConfig::getTilesInfo()
         */
 
         //Read config
-        string path = config["path"]; 
+        std::filesystem::path config_path(configPath);
+        string imagePath = config["path"];
+        string directoryPath = config_path.parent_path();
+        string path = directoryPath + "/" + imagePath; 
         int x = config["x"];
         int y = config["y"];
         bool rotate = config["rotate"];
 
         //Open image
 
-        cv::Mat img = cv::imread(configPath + path, cv::IMREAD_COLOR); 
+        cv::Mat img = cv::imread(path, cv::IMREAD_COLOR); 
 
         //Split image into tiles
         int tileWidth = img.cols / x;
         int tileHeight = img.rows / y;
+
+        vector<vector<cv::Mat>> tiles = splitImageIntoTiles(img, tileWidth, tileHeight);
+
+        std::map<cv::Mat, std::pair<int, int>, MatCompare> tileMap;
+        std::vector<std::vector<int>> tileIDs(tiles.size(), std::vector<int>(tiles[0].size()));
+        int nextID = 0;
+
+        for(int i = 0; i < tiles.size(); ++i)
+        {
+            for(int j = 0; j < tiles[i].size(); ++j)
+            {
+                auto& tile = tiles[i][j];
+                auto it = tileMap.find(tile);
+
+                if(it == tileMap.end())
+                {
+                    tileMap[tile] = {nextID, 1};
+                    tileIDs[i][j] = nextID;
+                    ++nextID;
+                }
+                else
+                {
+                    ++it->second.second;
+                    tileIDs[i][j] = it->second.first;
+                }
+            }
+        }
+
+
+        for(auto row : tileIDs)
+        {
+
+            for(auto tile : row)
+            {
+                std::cout << tile << ' ';
+            }
+            std::cout << '\n';
+        }
+        
 
         
 
@@ -59,6 +137,10 @@ vector<TileInfo> TilesetConfig::getTilesInfo()
 
 
 
+
+
+
+  
     }
 
     return res;
